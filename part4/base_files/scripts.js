3bcd2581cf291ac352a0658b8cedd9007253b84e
Matzuc2 <mathieu.zucalli2@holbertonschool.com>
2025-04-08 11:02:13 +0200

    Add backend configuration, database setup, and initial data
    - Created config.py for application configuration.
    - Added ER diagram and textual representation for database schema.
    - Implemented initial SQL data for users, places, amenities, and reviews.
    - Developed schema.sql for database structure creation.
    - Created setup_db.py to automate database setup.
    - Added test queries for database validation.
    - Initialized development database instance.
    - Implemented run.py to start the application.
    - Created comprehensive unit tests for API endpoints and SQLAlchemy configuration.
    - Updated frontend HTML files to enhance user interface and navigation.
    - Implemented JavaScript for login functionality and improved user experience.
    - Enhanced CSS styles for better visual presentation of place cards.

--- part4/base_files/scripts.js
+++ part4/base_files/scripts.js
@@ -4,5 +4,40 @@
 */
 
 document.addEventListener('DOMContentLoaded', () => {
-    /* DO SOMETHING */
+  const loginForm = document.getElementById('login-form');
+
+  if (loginForm) {
+      loginForm.addEventListener('submit', async (event) => {
+          event.preventDefault();
+          let email = document.getElementById("email");
+          let password = document.getElementById("password");
+
+          if (email.value == "" || password.value == "") {
+            alert("Ensure you input a value in both fields!");
+          } else {
+          loginUser(email.value, password.value)
+          console.log('Cookie set:', document.cookie);
+        }
+      });
+  }
   });
+
+  async function loginUser(email, password) {
+    const response = await fetch('http://127.0.0.1:5000/api/v1/auth/login', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({ email, password })
+    });
+    if (response.ok) {
+      const data = await response.json();
+      document.cookie = `token=${data.access_token}; path=/`;
+      window.location.href = 'index.html';
+      localStorage.setItem('token', data.access_token);
+      console.log('Stored token:', data.access_token);
+      } 
+      else {
+        alert('Login failed: ' + response.statusText);
+      }
+}
